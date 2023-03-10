a566f8383f0ee121a76af03df5c36466f70c3f5a
章珂 <zhangke@jimilab.com>
2023-03-10 10:45:30 +0800

    update

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Ike's Blog`;
-export const SITE_DESCRIPTION = 'Ike's Site Description';
+export const SITE_TITLE = "Ike's Blog";
+export const SITE_DESCRIPTION = "Ike's Site Description";
 export const SITE_EMAIL = '450639889@qq.com'
 export const SITE_NAME = 'ike.privateblog.com';
 export const SITE_URL = "https://ike.proviteblog.com";
