63ea3c553edf984fea50fd0ef8f52e1a1dd0ca95
ike <ikeno.7g6js@simplelogin.com>
2023-04-04 16:03:09 +0800

    update

--- public/static/js/initPost.js
+++ public/static/js/initPost.js
@@ -47,6 +47,17 @@ function initLazyLoad() {
       img.src = grandSon.src;
       let sign = md5(grandSon.src);
 
+      let target = document.getElementById(`lht${sign}`)
+      if (!target)  {
+          // If an absolute path is used as the image link, such as "/static/img.png",
+          // the URL of grandSon.src will become "https://example.com/static/img.png", resulting in a different md5.
+          // Therefore, we attempt to handle this situation by trying again with the absolute path.
+          const a = document.createElement('a');
+          a.href = grandSon.src;
+          sign = md5(a.pathname);
+      }
+
+
       img.onload = function () {
         let percent = ((img.height / img.width) * 100).toFixed(5);
         var style = document.createElement("style");
