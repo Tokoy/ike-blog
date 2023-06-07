9d2b31c8fe524870785c9446b5d07a680cc38867
ike <ikeno.7g6js@simplelogin.com>
2023-06-07 16:48:45 +0800

    update title

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "Ike の News";
+export const SITE_TITLE = "Ike の Blog";
 export const SITE_DESCRIPTION = "Ike's Blog,Memo someting interesting";
 export const SITE_EMAIL = 'ikeno.7g6js@simplelogin.com'
 export const SITE_NAME = 'blog.ikeno.top';
